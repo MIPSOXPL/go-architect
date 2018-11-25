94571388e637f24619feb3ff287d069cdca27b89
slawomir <kozokslawomir@gmail.com>
2018-11-25 23:10:33 +0100

    Modified recreate method for hierarchy. Modified test for recreate. Created test for copying of files in hierarchy.

--- helpers/hierarchyrecreator.go
+++ helpers/hierarchyrecreator.go
@@ -8,27 +8,48 @@ import (
 
 //HierarchyRecreator allows to recreate hierarchy
 type HierarchyRecreator struct {
+	savedPath          string
+	hierarchyGenerated bool
+	err                error
 }
 
-//Recreate allows to recreate hierarchy of hierarchy
-func (recreator *HierarchyRecreator) Recreate(hierarchy *RecursiveHierarchy, path string) error {
+//RecreateFolders allows to recreate hierarchy of hierarchy
+func (recreator *HierarchyRecreator) RecreateFolders(hierarchy *RecursiveHierarchy, path string) error {
+	recreator.hierarchyGenerated = false
 	if hierarchy.err != nil {
+		recreator.err = hierarchy.err
 		return hierarchy.err
 	}
 
-	return recreator.recreate(hierarchy, nil, path)
+	recreator.err = recreator.recreate(hierarchy, nil, path)
+
+	if recreator.err == nil {
+		recreator.savedPath = path
+		recreator.hierarchyGenerated = true
+	}
+
+	return recreator.err
 }
 
 func (recreator *HierarchyRecreator) recreate(hierarchy *RecursiveHierarchy, parent *resource.Folder, path string) error {
 
+	const userAccess = 0700
+
 	for counter := range hierarchy.folders {
 		actualFolder := hierarchy.folders[counter]
-
-		if actualFolder.GetParent() == parent {
+		actualParent := actualFolder.GetParent()
+		parentEqual := false
+		if actualParent == parent {
+			parentEqual = true
+		} else if actualParent != nil && parent != nil {
+			parentEqual = *actualParent == *parent
+		}
+		if parentEqual {
 			preparedPath := path + "/" + actualFolder.GetName()
-			err := os.Mkdir(preparedPath, os.ModeDir)
+			err := os.Mkdir(preparedPath, userAccess)
 
 			if err != nil {
+				recreator.err = err
 				return err
 			}
 
@@ -36,5 +57,10 @@ func (recreator *HierarchyRecreator) recreate(hierarchy *RecursiveHierarchy, par
 		}
 	}
 
+	return nil
+}
+
+//CopyFiles copies files after recreation of structure of folders
+func (recreator *HierarchyRecreator) CopyFiles(hierarchy *RecursiveHierarchy) error {
 	return nil
 }
--- helpers/hierarchyrecreator_test.go
+++ helpers/hierarchyrecreator_test.go
@@ -6,9 +6,11 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	const userAccess = 0700
+
 	outputPath := "../test-env/recursivehierarchy_output"
 	exitCode := 0
-	ok := os.MkdirAll(outputPath, os.ModeDir)
+	ok := os.MkdirAll(outputPath, userAccess)
 	if ok == nil {
 		exitCode = m.Run()
 	} else {
@@ -35,12 +37,44 @@ func TestHierarchyRecreation(test *testing.T) {
 	}
 
 	recreator := &HierarchyRecreator{}
-	recreator.Recreate(hierarchy, outputPath)
+	recreator.RecreateFolders(hierarchy, outputPath)
+
+	if recreator.err != nil {
+		test.Errorf("Hierarchy not recreated properly - error: %s\n", recreator.err.Error())
+	}
 
 	pathsToTest := []string{"folder1", "folderB", "folderB/folderA"}
 
 	for counter := 0; counter < len(pathsToTest); counter++ {
 		actualPath := outputPath + "/" + pathsToTest[counter]
+		if _, err := os.Stat(actualPath); os.IsNotExist(err) {
+			test.Errorf("Recreated path `%s` does not exist.", actualPath)
+		}
+	}
+}
+
+func TestHierarchyCopy(test *testing.T) {
+
+	testPath := "../test-env/recursivehierarchy"
+	outputPath := "../test-env/recursivehierarchy_output"
+
+	hierarchy := &RecursiveHierarchy{}
+	hierarchy.CreateHierarchy(testPath, nil)
+
+	if hierarchy.err != nil {
+		test.Errorf("Hierarchy getting due to test requirements failed:\n %s",
+			hierarchy.err.Error())
+	}
+
+	recreator := &HierarchyRecreator{}
+	recreator.RecreateFolders(hierarchy, outputPath)
+	recreator.CopyFiles(hierarchy)
+
+	elementsToTest := []string{"fileA.txt", "folder1/fileC.txt",
+		"folderB/fileX.txt", "folderB/folderA/fileY.txt"}
+
+	for counter := 0; counter < len(elementsToTest); counter++ {
+		actualPath := outputPath + "/" + elementsToTest[counter]
 		if _, err := os.Stat(actualPath); os.IsNotExist(err) {
 			test.Errorf("Recreated path `%s` does not exist.", actualPath)
 		}
